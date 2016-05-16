e4fae2d66441e8acba372017582a526f697f941f
Dominic Reich <dominic@mm.st>
2016-05-17 00:37:23 +0200

    build.sh: combine both commands with '&&'   * if first command fails, second does not get executed

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-g++ -O2 -pipe -fomit-frame-pointer -Wall -s -pie -fpie -lboost_system -o blacklist blacklist.cpp
-sudo cp blacklist /usr/local/bin/
+g++ -O2 -pipe -fomit-frame-pointer -Wall -s -pie -fpie -lboost_system -o blacklist blacklist.cpp && cp blacklist /usr/local/bin/
